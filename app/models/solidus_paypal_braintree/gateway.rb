6d1c1a5f4d5fdd3833c76b926b3cb113bc2f126b
Evgeniy Denisov <lifeemulation@gmail.com>
2022-07-01 18:24:00 +0300

    Fix fetching braintree_paypal_id from line items

--- app/models/solidus_paypal_braintree/gateway.rb
+++ app/models/solidus_paypal_braintree/gateway.rb
@@ -106,7 +106,7 @@ module SolidusPaypalBraintree
     def purchase(money_cents, payment_source, gateway_options)
       decreased_amount = money_cents
       responses = []
-      plan_ids = line_items_with_subscription(gateway_options).pluck(:braintree_plan_id)
+      plan_ids = line_items_with_subscription(gateway_options).map(&:braintree_plan_id).map(&:presence).compact
       if plan_ids.any?
         decreased_amount = decrease_amount(gateway_options, money_cents)
         responses.push create_subscriptions(payment_source, gateway_options, plan_ids)
